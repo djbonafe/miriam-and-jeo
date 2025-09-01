c53badf0224b639747f76ad4b6b4abd6ee6d8f9b
djbonafe <Dominic.Jisum.S.Bonafe@gds.ey.com>
2025-09-01 11:17:00 +0800

    added entourage details

--- src/pages/WeddingEntourage.jsx
+++ src/pages/WeddingEntourage.jsx
@@ -3,38 +3,77 @@ import React from 'react'
 export default function WeddingEntourage() {
   return (
     <div>
+        <div className='entourage-heading-box'>
+      <h1 className='entourage-heading'>The Entourage</h1>
+      <h1 className='ocampo-antone'>OCAMPO - ANTONE</h1>
+      <h2 className='nuptials'>Nuptials</h2>
+      </div>
+      {/* <div>Officiating Priest</div>
+      <div>Name of Priest</div> */}
 
-        <div>The entourage</div>
-        <div>OCAMPO - ANTONE</div>
-        <div>nuptials</div>
-        <div>Officating Priest</div>
-        <div>name of priest</div>
-        <div className='best-honor-box'>
-            <div>
-                <h3>Best Man</h3>
-                <p>Israel Ocampo</p>
-            </div>
-            <div><h3>Maid of Honor</h3>
-                <p>Ana Grace Antone</p></div>
+      <div className='best-honor-box'>
+        <div>
+          <h3 className='entourage-header'>Best Man</h3>
+          <p>Israel Ocampo</p>
         </div>
         <div>
-            <div className='groomsmen-box'>
-                <h3>Groomsmen</h3>
-                <p>Daniel Ocampo</p>
-                <p>Ronnie Barba</p>
-                <p>Brant Manigos</p>
-                <p>Ivan Thomas</p>
-                <p>Jusi Von Ordona/Pranaya</p>
-            </div>
-            <div className='bridesmaids-box'>
-                <h3>Bridesmaids</h3>
-                <p>Trixie Ann Ca-aya</p>
-                <p>Jamby Rizaba</p>
-                <p>Orbe Ebro</p>
-                <p>Ma. Ayehssa Valenzuela</p>
-                <p>Joyce Infante </p>
-            </div>
+          <h3 className='entourage-header'>Maid of Honor</h3>
+          <p>Ana Grace Antone</p>
         </div>
+      </div>
+
+      <div className='party-box'>
+        <div className='groomsmen-box'>
+          <h3 className='entourage-header'>Groomsmen</h3>
+          <p>Daniel Ocampo</p>
+          <p>Ronnie Barba</p>
+          <p>Brant Manigos</p>
+          <p>Ivan Thomas</p>
+          <p>Jusi Von Ordona/Pranaya</p>
+        </div>
+
+        <div className='bridesmaids-box'>
+          <h3 className='entourage-header'>Bridesmaids</h3>
+          <p>Trixie Ann Ca-aya</p>
+          <p>Jamby Rizaba</p>
+          <p>Orbe Ebro</p>
+          <p>Ma. Ayehssa Valenzuela</p>
+          <p>Joyce Infante</p>
+        </div>
+      </div>
+
+      <div className='flowergirls-box'>
+        <h3 className='entourage-header'>Flowergirls</h3>
+        <p>Kaitlyn Fate Sacudit</p>
+        <p>Lakiesha Ordona</p>
+        <p>Sierra Jasmin</p>
+        <p>Eliana Gabriel Ocampo</p>
+      </div>
+
+      <div className='bearers-box'>
+        <div>
+        <h3 className='entourage-header'>Coin Bearer</h3>
+        <p>Raymonda Sacudit</p>
+        </div>
+        <div>
+        <h3 className='entourage-header'>Ring Bearer</h3>
+        <p>Ethan Chase Jasmin</p>
+        </div>
+      </div>
+
+      <div className='principal-sponsors-box'>
+        <h3 className='entourage-header'>Principal Sponsors</h3>
+        <p>Irene Desamero</p>
+        <p>Juliet Palabasan</p>
+        <p>Helen Bonafe</p>
+        <p>Rene Bonafe</p>
+        <p>Jasper Ocampo</p>
+        <p>Lyn Bonifacio</p>
+        <p>Michael Ocampo</p>
+        <p>Ganebie Ocampo</p>
+        <p>Erlinda Antone</p>
+        <p>Anacorita Togle</p>
+      </div>
     </div>
   )
 }
